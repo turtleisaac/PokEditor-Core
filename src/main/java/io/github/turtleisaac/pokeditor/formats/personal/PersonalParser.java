752ebdc1baee397fac03b37efa69d6f0ad0d3096
turtleisaac <turtleisaac1@gmail.com>
2023-12-05 18:17:15 -0600

    added editing of TM icon palettes for proper types, added missing pointers to items and tm/hm tables

--- src/main/java/io/github/turtleisaac/pokeditor/formats/personal/PersonalParser.java
+++ src/main/java/io/github/turtleisaac/pokeditor/formats/personal/PersonalParser.java
@@ -25,6 +25,7 @@ import io.github.turtleisaac.nds4j.binaries.CodeBinary;
 import io.github.turtleisaac.nds4j.framework.Endianness;
 import io.github.turtleisaac.nds4j.framework.MemBuf;
 import io.github.turtleisaac.pokeditor.formats.BytesDataContainer;
+import io.github.turtleisaac.pokeditor.formats.moves.MoveData;
 import io.github.turtleisaac.pokeditor.gamedata.GameCodeBinaries;
 import io.github.turtleisaac.pokeditor.gamedata.GameFiles;
 import io.github.turtleisaac.pokeditor.formats.GenericParser;
@@ -38,7 +39,9 @@ import java.util.Map;
 public class PersonalParser implements GenericParser<PersonalData>
 {
     public static final int[] tmMoveIdNumbers = new int[PersonalData.NUMBER_TMS_HMS];
+    public static final int[] tmMoveTypes = new int[PersonalData.NUMBER_TMS_HMS];
     private static boolean hasReadTmMoveIds = false;
+    private static boolean hasReadTmMoveTypes = false;
 
     @Override
     public List<PersonalData> generateDataList(Map<GameFiles, Narc> narcs, Map<GameCodeBinaries, CodeBinary> codeBinaries)
@@ -73,6 +76,19 @@ public class PersonalParser implements GenericParser<PersonalData>
                 tmMoveIdNumbers[i] = reader.readUInt16();
             }
             hasReadTmMoveIds = true;
+
+            reader.setPosition(Tables.ITEMS.getPointerOffset());
+            offset = reader.readInt();
+            reader.setPosition(offset - arm9.getRamStartAddress());
+            reader.skip(2*4*TM01_INDEX);
+            for (int i = 0; i < PersonalData.NUMBER_TMS_HMS; i++)
+            {
+                reader.skip(4);
+                tmMoveTypes[i] = tmHmPaletteIndexToType(reader.readUInt16());
+                reader.skip(2);
+            }
+
+            hasReadTmMoveTypes = true;
         } finally {
             arm9.unlock();
         }
@@ -111,6 +127,28 @@ public class PersonalParser implements GenericParser<PersonalData>
                 for (int i = 0; i < PersonalData.NUMBER_TMS_HMS; i++)
                 {
                     writer.writeShort((short) tmMoveIdNumbers[i]);
+                }
+            } finally {
+                arm9.unlock();
+            }
+        }
+
+        if (hasReadTmMoveTypes)
+        {
+            CodeBinary arm9 = codeBinaries.get(GameCodeBinaries.ARM9);
+            arm9.lock();
+            try {
+                MemBuf.MemBufReader reader = arm9.getPhysicalAddressBuffer().reader();
+                reader.setPosition(Tables.ITEMS.getPointerOffset());
+                int offset = reader.readInt();
+                MemBuf.MemBufWriter writer = arm9.getPhysicalAddressBuffer().writer();
+                writer.setPosition(offset - arm9.getRamStartAddress());
+                writer.skip(2*4*TM01_INDEX);
+                for (int i = 0; i < PersonalData.NUMBER_TMS_HMS; i++)
+                {
+                    writer.skip(4);
+                    writer.writeShort((short) typeToTmHmPaletteIndex(tmMoveTypes[i]));
+                    writer.skip(2);
                 }
             } finally {
                 arm9.unlock();
@@ -156,4 +194,63 @@ public class PersonalParser implements GenericParser<PersonalData>
 //            jsonGenerator.writeEndObject();
 //        }
 //    }
+
+    private static final int TM01_INDEX = 328;
+
+    private static int tmHmPaletteIndexToType(int paletteIndex)
+    {
+        return switch(paletteIndex) {
+            case 0x18E -> 1; // fighting
+            case 0x18F -> 16; // dragon
+            case 0x190 -> 11; // water
+            case 0x191 -> 14; // psychic
+            case 0x192 -> 0; // normal
+            case 0x193 -> 3; // poison
+            case 0x194 -> 15; // ice
+            case 0x195 -> 12; // grass
+            case 0x196 -> 10; // fire
+            case 0x197 -> 17; // dark
+            case 0x198 -> 8; // steel
+            case 0x199 -> 13; // electric
+            case 0x19A -> 4; // ground
+            case 0x19B -> 7; // ghost
+            case 0x19C -> 5; // rock
+            case 0x19D -> 2; // flying
+            case 0x262 -> 6; // bug
+            default -> 0;
+            //todo figure out how to handle ???/fairy
+        };
+    }
+
+    private static int typeToTmHmPaletteIndex(int type)
+    {
+        return switch(type) {
+            case 1 -> 0x18E; // fighting
+            case 16 -> 0x18F; // dragon
+            case 11 -> 0x190; // water
+            case 14 -> 0x191; // psychic
+            case 0 -> 0x192; // normal
+            case 3 -> 0x193; // poison
+            case 15 -> 0x194; // ice
+            case 12 -> 0x195; // grass
+            case 10 -> 0x196; // fire
+            case 17 -> 0x197; // dark
+            case 8 -> 0x198; // steel
+            case 13 -> 0x199; // electric
+            case 4 -> 0x19A; // ground
+            case 7 -> 0x19B; // ghost
+            case 5 -> 0x19C; // rock
+            case 2 -> 0x19D; // flying
+            case 6 -> 0x262; // bug
+            case 9 -> 0x191; // ???/fairy
+            default -> 0x192;
+            //todo figure out how to handle ???/fairy
+        };
+    }
+
+    public static void updateTmType(int tmID, int moveID, List<MoveData> moves)
+    {
+        tmMoveIdNumbers[tmID] = moveID;
+        tmMoveTypes[tmID] = moves.get(moveID).getType();
+    }
 }
--- src/main/java/io/github/turtleisaac/pokeditor/gamedata/Tables.java
+++ src/main/java/io/github/turtleisaac/pokeditor/gamedata/Tables.java
@@ -36,42 +36,50 @@ public enum Tables
                         PARTY_ICON_PALETTE.pointerOffset = 0x079f80;
                         TRAINER_CLASS_PRIZE_MONEY.pointerOffset = 0x816c;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x793b4;
+                        ITEMS.pointerOffset = 0x7cef4;
                         TM_HM_MOVES.pointerOffset = 0x07d288;
                     }
-                    case GERMANY -> {
+                    case GERMANY, FRANCE -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x07a020;
 //                        TRAINER_CLASS_PRIZE_MONEY.pointerOffset = ;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x079454;
+                        ITEMS.pointerOffset = 0x7cf94;
+                        TM_HM_MOVES.pointerOffset = 0x7d328;
                     }
                     case EUROPE -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x079f80;
 //                        TRAINER_CLASS_PRIZE_MONEY.pointerOffset = ;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x0793b4;
-                    }
-                    case FRANCE -> {
-                        PARTY_ICON_PALETTE.pointerOffset = 0x07a020;
-//                        TRAINER_CLASS_PRIZE_MONEY.pointerOffset = ;
-                        TRAINER_CLASS_GENDER.pointerOffset = 0x079454;
+                        ITEMS.pointerOffset = 0x7cef4;
+                        TM_HM_MOVES.pointerOffset = 0x7d288;
                     }
                     case SPAIN -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x07a020;
 //                        TRAINER_CLASS_PRIZE_MONEY.pointerOffset = ;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x079454;
+                        ITEMS.pointerOffset = 0x7cf94;
+                        TM_HM_MOVES.pointerOffset = 0x7d328;
                     }
                     case ITALY -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x07a020;
 //                        TRAINER_CLASS_PRIZE_MONEY.pointerOffset = ;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x079454;
+                        ITEMS.pointerOffset = 0x7cf94;
+                        TM_HM_MOVES.pointerOffset = 0x7d328;
                     }
                     case JAPAN -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x079858;
 //                        TRAINER_CLASS_PRIZE_MONEY.pointerOffset = ;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x078c8c;
+                        ITEMS.pointerOffset = 0x7c7c4;
+                        TM_HM_MOVES.pointerOffset = 0x7cb58;
                     }
                     case KOREA -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x07a46c;
 //                        TRAINER_CLASS_PRIZE_MONEY.pointerOffset = ;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x0798a0;
+                        ITEMS.pointerOffset = 0x7d3e0;
+                        TM_HM_MOVES.pointerOffset = 0x7d774;
                     }
                 }
             }
@@ -79,77 +87,93 @@ public enum Tables
                 PARTY_ICON_PALETTE.pointerLocation = GameCodeBinaries.ARM9;
                 TRAINER_CLASS_GENDER.pointerLocation = GameCodeBinaries.ARM9;
                 TM_HM_MOVES.pointerLocation = GameCodeBinaries.ARM9;
+                ITEMS.pointerLocation = GameCodeBinaries.ARM9;
                 switch (baseROM.getRegion()) {
                     case USA -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074408;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c94;
                         TM_HM_MOVES.pointerOffset = 0x078020;
                     }
-                    case GERMANY -> {
+                    case GERMANY, EUROPE, FRANCE, ITALY -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074408;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
-                    }
-                    case EUROPE -> {
-                        PARTY_ICON_PALETTE.pointerOffset = 0x074408;
-                        TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
-                    }
-                    case FRANCE -> {
-                        PARTY_ICON_PALETTE.pointerOffset = 0x074408;
-                        TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c94;
+                        TM_HM_MOVES.pointerOffset = 0x78020;
                     }
                     case SPAIN -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074400;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x0735f8;
-                    }
-                    case ITALY -> {
-                        PARTY_ICON_PALETTE.pointerOffset = 0x074408;
-                        TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c8c;
+                        TM_HM_MOVES.pointerOffset = 0x78018;
                     }
                     case JAPAN -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x073ea0;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073098;
+                        ITEMS.pointerOffset = 0x77724;
+                        TM_HM_MOVES.pointerOffset = 0x77ab0;
                     }
                     case KOREA -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074508;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073700;
+                        ITEMS.pointerOffset = 0x77d94;
+                        TM_HM_MOVES.pointerOffset = 0x78120;
                     }
                 }
             }
             case SoulSilver -> {
                 PARTY_ICON_PALETTE.pointerLocation = GameCodeBinaries.ARM9;
                 TRAINER_CLASS_GENDER.pointerLocation = GameCodeBinaries.ARM9;
+                TM_HM_MOVES.pointerLocation = GameCodeBinaries.ARM9;
+                ITEMS.pointerLocation = GameCodeBinaries.ARM9;
                 switch (baseROM.getRegion()) {
                     case USA -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074408;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c94;
+                        TM_HM_MOVES.pointerOffset = 0x78020;
                     }
                     case GERMANY -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074408;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c94;
+                        TM_HM_MOVES.pointerOffset = 0x78020;
                     }
                     case EUROPE -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074408;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c94;
+                        TM_HM_MOVES.pointerOffset = 0x78020;
                     }
                     case FRANCE -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074408;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c94;
+                        TM_HM_MOVES.pointerOffset = 0x78020;
                     }
                     case SPAIN -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074408;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c94;
+                        TM_HM_MOVES.pointerOffset = 0x78020;
                     }
                     case ITALY -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074408;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073600;
+                        ITEMS.pointerOffset = 0x77c94;
+                        TM_HM_MOVES.pointerOffset = 0x78020;
                     }
                     case JAPAN -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x073ea0;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x073098;
+                        ITEMS.pointerOffset = 0x77724;
+                        TM_HM_MOVES.pointerOffset = 0x77ab0;
                     }
                     case KOREA -> {
                         PARTY_ICON_PALETTE.pointerOffset = 0x074500;
                         TRAINER_CLASS_GENDER.pointerOffset = 0x0736f8;
+                        ITEMS.pointerOffset = 0x77d8c;
+                        TM_HM_MOVES.pointerOffset = 0x78118;
                     }
                 }
             }
